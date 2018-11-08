191a518d4bb0c88631c1f39e626c07e4dab5f358
hiroya7777 <44703354+hiroya7777@users.noreply.github.com>
2018-11-08 23:39:53 +0900

    Add files via upload

---
+++ ruby02 kadai.rb
@@ -0,0 +1,31 @@
+1.puts "初めてのRuby"
+2.puts "Ruby"+"始めました"
+3. 6 0 9 1
+4. [1,2,3,4]
+5. num_array = [1,2,3,4]
+6. p num_array[0]
+   p num_array[1]
+   p num_array[2]
+   p num_array[3]
+
+7. kawaguchi = { name: 'hiroya kawaguchi',birthday: "1992/11/17",bload: "B" }
+
+8.def plus_ruby
+   p "like"+"ruby"
+  end
+   plus_ruby
+
+  def plus_ruby
+   p "yhee"+"ruby"
+  end
+   plus_ruby
+
+9.def plus_one
+   p 4.next
+  end
+  plus_one
+
+  def plus_one
+   p 10.next
+  end
+   plus_one
